3d4b7e3f0d24eec615b418e46127048c47a4953f
Jae Shin Ryu <Jae Shin Ryu>
2016-07-16 12:49:33 -0400

    added all the solutions

--- algo/reverse_a_string.rb
+++ algo/reverse_a_string.rb
@@ -1,8 +1,11 @@
-# Complete the method called reverse_a_string that accepts a string as a parameter and 
+# Complete the method called reverse_a_string that accepts a string as a parameter and
 # returns the reverse. The one caveat: Don't use the reverse method that already
 # comes with Ruby!
 
 def reverse_a_string(string)
+  letters = string.split(//)
+  reverse = letters.sort! {|a,b| b <=> a}
+  result = reverse.join
 
 end
 
